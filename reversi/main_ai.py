55439cadf785dcd0de78c99f524c47ff433425bf
jacecao <280749498@qq.com>
2018-08-13 22:21:18 +0800

    add AI player

--- reversi/main_ai.py
+++ reversi/main_ai.py
@@ -1,10 +1,5 @@
 # 64格棋盘游戏
-# 相比Tic Tac Toe更厉害的AI
-# 
-# 游戏玩法：
-# X O 代表双方落子，在不同方向中只要一方中间的对方落子就会变为己方落子
-# 例如 X O O X  => X X X X
-# 最终谁的棋子多谁获胜
+# 电脑和电脑玩
 
 import random
 import sys
@@ -100,14 +95,17 @@ def isValidMove(board, tile, xstart, ystart):
 			# 那么继续按照该方向搜寻有没有对方的落子
 			x += xdirection
 			y += ydirection
+			
 			if not isOnBoard(x, y):
 				continue
+			
 			while board[x][y] == otherTile:
 				x += xdirection
 				y += ydirection
 				# 直到该方向的落子搜寻完毕 退出寻找
 				if not isOnBoard(x, y):
 					break
+			
 			# 如果该方向棋盘格找完退出本次循环，进入下一个循环
 			# 也就是结束该方向的寻找，进入下一个方向搜索
 			if not isOnBoard(x, y):
@@ -119,29 +117,22 @@ def isValidMove(board, tile, xstart, ystart):
 				while True:
 					x -= xdirection
 					y -= ydirection
+					
 					if x == xstart and y == ystart:
 						break
+					
 					tilesToFlip.append([x, y])
 	# 清空落子点
 	board[xstart][ystart] = ' '	
+	
 	if len(tilesToFlip) == 0:
 		return False
+	
 	return tilesToFlip						
 
 # 检查落子是否在棋盘内
 def isOnBoard(x, y):
 	return x >= 0 and x <=7 and y >=0 and y <=7
-
-
-# 获取落子提示棋盘数据
-# 也就是这里返回的棋盘数据包含下一步的落子提示
-def getBoardWithValidMoves(board, tile):
-	dupeBoard = getBoardCopy(board)
-
-	for x, y in getValidMoves(dupeBoard, tile):
-		dupeBoard[x][y] = '.'
-
-	return dupeBoard
 
 # 获取有效落子坐标
 def getValidMoves(board, tile):
@@ -167,19 +158,6 @@ def getScoreOfBoard(board):
 				o_scroe += 1
 
 	return {'X': x_scroe, 'O': o_scroe}	
-
-# 输入玩家棋子显示方式
-def enterPlayerTile():
-	tile = ''
-	while not (tile == 'X' or tile == 'O'):
-		print('Do you want to be X or O ?')
-		tile = input().upper()	
-
-	# [代表玩家落子, 代表电脑落子]	
-	if tile == 'X':
-		return ['X', 'O']
-	if tile == 'O':
-		return ['O', 'X']	
 
 # 执行棋盘落子
 def makeMove(board, tile, xstart, ystart):
@@ -208,53 +186,17 @@ def makeMove(board, tile, xstart, ystart):
 	for x, y in tilesToFlip:
 		board[x][y] = tile
 
-	# 打印落子坐标
-	print('**** Move To X=%s Y=%s ****', (xstart, ystart)) 
-
 	return True	
 
 # 判断落子坐标是否在棋盘四个角上
 def isOnCorner(x, y):
 	return (x == 0 and y == 0) or (x == 7 and y == 0) or (x == 0 and y == 7) or (x == 7 and y == 7)
 
-# 获取和处理玩家落子
-def getPlayerMove(board, playerTile):
-	
-	DIGITS1TO8 = '1 2 3 4 5 6 7 8'.split()
-
-	while True:
-		# 玩家完整需要执行的操作
-		# 1. 输入落子坐标  
-		# 2. 输入 quit / q 退出游戏   
-		# 3. 输入hints / h 显示提示 
-		print('Enter your move, or type "quit" to end the game, or type "hints" turn off/on hints.')
-		move = input().lower()
-		if move == 'quit' or move == 'q':
-			return 'quit'
-		if move == 'hints' or move == 'h':
-			return 'hints'	
-		# 检查输入的坐标值是否正确	
-		if len(move) == 2 and move[0] in DIGITS1TO8 and move[1] in DIGITS1TO8:
-			# 玩家输入的坐标范围是1-8，而实际程序中是1-7
-			# 所以这里需要减1
-			x = int(move[0]) - 1
-			y = int(move[1]) - 1
-
-			if isValidMove(board, playerTile, x, y) == False:
-				continue
-			else:
-			 	break
-		else:
-			print('!!!!!!!!!!!')
-			print('That is not a valid move, Type the x digit (1-8) the y digit(1-8).')
-			print('For example , 81 will be the top-right corner.')
-
-	return [x, y]
 
 # 获取计算机落子
 def getComputerMove(board, computerTile):
 	possibleMoves = getValidMoves(board, computerTile)
-	print(possibleMoves)
+	# print(possibleMoves)
 	# 对可以落子的点进行随机排序
 	random.shuffle(possibleMoves)
 
@@ -280,8 +222,8 @@ def getComputerMove(board, computerTile):
 def showPoints(board, playerTile, computerTile):
 	scores = getScoreOfBoard(board)
 	print()
-	print('***** POINT ******')
-	print('You have %s points, the computer has %s points.' % (scores[playerTile], scores[computerTile]))
+	print('           ***** POINT ******')
+	print('    %s has %s points, %s has %s points.' % (playerTile, scores[playerTile], computerTile, scores[computerTile]))
 
 
 # 随机选择起始落子方
@@ -303,63 +245,44 @@ def init():
 	while True:
 		mainBoard = createNewBoard()
 		resetBoard(mainBoard)
-		playerTile, computerTile = enterPlayerTile()
 		showHints = False
-		turn = whoGosFirst()
+		if whoGosFirst() == 'player':
+			turn = 'X'
+		else:
+			turn = 'O'	
 
 		print()
 		print('The [' + turn + '] will go first')
 
 		while True:
-			if turn == 'player':
-				# 玩家落子执行代码
-				if showHints:
-					validMovesBoard = getBoardWithValidMoves(mainBoard, playerTile)
-					drawBoard(validMovesBoard)
-				else:
-					drawBoard(mainBoard)
-				showPoints(mainBoard, playerTile, computerTile)	
-
-				move = getPlayerMove(mainBoard, playerTile)
-				if move == 'quit' or move == 'q':
-					print('Thanks for playing!')
-					sys.exit()
-				elif move == 'hints' or move == 'h':
-					showHints = not showHints
-					continue
-				else:
-					makeMove(mainBoard, playerTile, move[0], move[1])
+			if turn == 'X':
+				otherTile = 'O'
+				x, y = getComputerMove(mainBoard, turn)
+				makeMove(mainBoard, turn, x, y)
 
 				# 检查电脑是否有落子的机会，如果没有就跳出程序
 				# 如果有就轮到电脑落子	
-				if getValidMoves(mainBoard, computerTile) == []:
+				if getValidMoves(mainBoard, otherTile) == []:
 					break
 				else:
-					turn = 'computer'				
+					turn = 'O'				
 
 			else:
 				# 电脑落子
-				drawBoard(mainBoard)
-				showPoints(mainBoard, playerTile, computerTile)
-				input('Press [ENTER] to see the computer\'s move.')
-				x, y = getComputerMove(mainBoard,computerTile)
-				makeMove(mainBoard, computerTile, x, y)
+				# 显示棋盘
+				turn = 'O'
+				otherTile = 'X'
+				x, y = getComputerMove(mainBoard, turn)
+				makeMove(mainBoard, turn, x, y)
 
-				if getValidMoves(mainBoard, playerTile) == []:
+				if getValidMoves(mainBoard, otherTile) == []:
 					break
 				else:
-					turn = 'player'	
+					turn = 'X'	
 
 		drawBoard(mainBoard)
 		scores = getScoreOfBoard(mainBoard)
-		showPoints(mainBoard, playerTile, computerTile)
-
-		if scores[playerTile] > scores[computerTile]:
-			print('You beat the computed by %s points! Great Job' % (scores[playerTile] - scores[computerTile]))
-		elif scores[playerTile] < scores[computerTile]:
-			print('You lost. the computed beat you by %s points! ' % (scores[computerTile] - scores[playerTile]))
-		else:
-			print('The Game was a tie !')
+		showPoints(mainBoard, 'X', 'O')
 
 		if not playAgin():
 			break
